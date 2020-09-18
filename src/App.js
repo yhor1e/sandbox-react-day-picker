efe9a9604c51062f1dfc165fc75228b74539773d
yhorie <yuk.horie@gmail.com>
2020-09-18 23:08:37 +0900

    Add modifiers

--- src/App.js
+++ src/App.js
@@ -1,16 +1,32 @@
 import React from 'React';
 import DayPickerInput from 'react-day-picker/DayPickerInput';
 import 'react-day-picker/lib/style.css';
-import MomentLocaleUtils from 'react-day-picker/moment';
+import './App.css';
+import MomentLocaleUtils, {
+  formatDate,
+  parseDate,
+} from 'react-day-picker/moment';
 import 'moment/locale/ja';
 
 function renderDay(day) {
   return <div title="title">{day.getDate()}</div>;
 }
 
+const modifiers = {
+  sun: { daysOfWeek: [0] },
+  sat: { daysOfWeek: [6] },
+  holiday: [new Date('2020-09-30'), new Date('2020-09-25')],
+  monday: { daysOfWeek: [1] },
+};
+
 const App = (props) => (
   <DayPickerInput
+    hideOnDayClick={false}
+    formatDate={formatDate}
+    parseDate={parseDate}
     dayPickerProps={{
+      modifiers: modifiers,
+      disabledDays: new Date(),
       localeUtils: MomentLocaleUtils,
       locale: 'ja',
       renderDay: renderDay,
